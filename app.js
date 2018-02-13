8713e7de1026f34d22dced90e09c6e1c0d14326c
Sasho Najdov <snajdov@gmail.com>
2018-02-13 09:43:50 +0100

    Enable CORS

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const mysql = require('mysql');
 
 const app = express();
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const connection = mysql.createConnection({
   host: 'iltgroup3.cp6f73a0po5w.eu-central-1.rds.amazonaws.com',
   user: 'writeuser',
@@ -26,10 +33,17 @@ app.get('/insert-test', (req, res) => {
     });
 })
 
+app.get('/getTeams', (req, res) => {
+  connection.query('SELECT name FROM teams;', function(error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  })
+})
+
 app.get('/goodbye', (req, res) => {
   res.send('Goodbye World!')
 })
 
 
 
-app.listen(8081, () => console.log('Example app listening on port 3000!'))
+app.listen(8081, () => console.log('Example app listening on port 8081!'))
